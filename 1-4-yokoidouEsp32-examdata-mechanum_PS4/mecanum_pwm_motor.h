b5359c4be1f1ee0d1ed2408c85ef12c10a8ec6a8
OtaniMasaki <tackey-kumachan@fuji.waseda.jp>
2024-11-13 19:06:44 +0900

    changed the Pin Set Up
    changed the Pin Set Up to confit mechanam moter

--- 1-4-yokoidouEsp32-examdata-mechanum_PS4/mecanum_pwm_motor.h
+++ 1-4-yokoidouEsp32-examdata-mechanum_PS4/mecanum_pwm_motor.h
@@ -1,5 +1,160 @@
 // 参考：https://rikoubou.hatenablog.com/entry/2017/06/05/172953
+#include <Arduino.h>
+#include <PS4Controller.h>
 
+//uint32_t ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
+//void ledcAttachPin(uint8_t pin, uint8_t channel);
+//void ledcWrite(uint8_t channel, uint32_t duty);
+
+/* 使うピンの定義 */
+// 左前車輪
+const int LEFT_FRONT_IN1 = 23;
+const int LEFT_FRONT_IN2 = 22;
+const int LEFT_FRONT_PWM = 21;
+
+// 左後車輪
+const int LEFT_BACK_IN1 = 1;
+const int LEFT_BACK_IN2 = 3;
+const int LEFT_BACK_PWM = 5;
+
+// 右前車輪
+const int RIGHT_FRONT_IN1 = 4;
+const int RIGHT_FRONT_IN2 = 16;
+const int RIGHT_FRONT_PWM = 17;
+
+// 右後車輪
+const int RIGHT_BACK_IN1 = 15;
+const int RIGHT_BACK_IN2 = 2;
+const int RIGHT_BACK_PWM = 18;
+
+/* チャンネルの定義 */
+const int CHANNEL_LEFT_FRONT = 0;
+const int CHANNEL_LEFT_BACK = 1;
+const int CHANNEL_RIGHT_FRONT = 2;
+const int CHANNEL_RIGHT_BACK = 3;
+
+const int LEDC_TIMER_BIT = 8;   // PWMの範囲(8bitなら0〜255、10bitなら0〜1023)
+const int LEDC_BASE_FREQ = 490; // 周波数(Hz)
+const int VALUE_MAX = 255;      // PWMの最大値
+
+// 関数のプロトタイプ宣言
+void frontLeftMotor(int32_t pwm);
+void backLeftMotor(int32_t pwm);
+void frontRightMotor(int32_t pwm);
+void backRightMotor(int32_t pwm);
+void brake();
+void coast();
+void LetsMoveMoter(int32_t pwm, double Cont_A, double Cont_B);
+void CalculateCont(double &Cont_A, double &Cont_B);
+
+void init_pwm_setup() {
+  // 各ピンを出力モードに設定
+  pinMode(LEFT_FRONT_IN1, OUTPUT);
+  pinMode(LEFT_FRONT_IN2, OUTPUT);
+  pinMode(LEFT_BACK_IN1, OUTPUT);
+  pinMode(LEFT_BACK_IN2, OUTPUT);
+  pinMode(RIGHT_FRONT_IN1, OUTPUT);
+  pinMode(RIGHT_FRONT_IN2, OUTPUT);
+  pinMode(RIGHT_BACK_IN1, OUTPUT);
+  pinMode(RIGHT_BACK_IN2, OUTPUT);
+
+  // 各チャンネルに対してPWMの設定
+  ledcSetup(CHANNEL_LEFT_FRONT, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+  ledcSetup(CHANNEL_LEFT_BACK, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+  ledcSetup(CHANNEL_RIGHT_FRONT, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+  ledcSetup(CHANNEL_RIGHT_BACK, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
+
+  // 各PWMピンをチャンネルにアタッチ
+  ledcAttachPin(LEFT_FRONT_PWM, CHANNEL_LEFT_FRONT);
+  ledcAttachPin(LEFT_BACK_PWM, CHANNEL_LEFT_BACK);
+  ledcAttachPin(RIGHT_FRONT_PWM, CHANNEL_RIGHT_FRONT);
+  ledcAttachPin(RIGHT_BACK_PWM, CHANNEL_RIGHT_BACK);
+}
+
+//
+void LetsMoveMoter(int32_t pwm, double Cont_A, double Cont_B) {
+  const int VALUE_MAX = 255;
+  double absv=sqrt((PS4.LStickX()^2)+(PS4.LStickY()^2))*(VALUE_MAX/128);
+  if (pwm > VALUE_MAX || pwm < -VALUE_MAX) pwm = VALUE_MAX;
+
+  double directions[4] = {Cont_A, Cont_B, -Cont_B, -Cont_A};
+  frontLeftMotor(absv * directions[0]);
+  frontRightMotor(absv * directions[1]);
+  backRightMotor(absv * directions[2]);
+  backLeftMotor(absv * directions[3]);
+  //これで呼び出せているはず
+  //1が正典、-1が反転
+}
+
+void CalculateCont(double &Cont_A, double &Cont_B) {
+  int PS4_LStickX = PS4.LStickX();
+  int PS4_LStickY = PS4.LStickY();
+
+  double absv=sqrt((PS4_LStickX^2)+(PS4_LStickY^2));
+
+  Serial.printf("Left Stick x at %d\n", PS4_LStickX);
+  Serial.printf("Left Stick y at %d\n", PS4_LStickY);
+
+  // 調整の部分
+  double cos_theta = PS4_LStickX / (absv);//Vxの向き成分
+  double sin_theta = PS4_LStickY / (absv);//Vyの向き成分（絶対値かけたら速度。VALUW＿MAXに絶対値を入れ混む⇒小さくなりすぎる）
+
+  Cont_A=-(cos_theta+sin_theta)/sqrt(2);
+  Cont_B=-(cos_theta-sin_theta)/sqrt(2);
+  //
+}
+
+//この先は各車輪ごとの制御
+
+// 左前車輪の制御
+void frontLeftMotor(int32_t pwm) {
+  if (pwm >= 0) {
+    digitalWrite(LEFT_FRONT_IN1, HIGH);
+    digitalWrite(LEFT_FRONT_IN2, LOW);
+  } else {
+    digitalWrite(LEFT_FRONT_IN1, LOW);
+    digitalWrite(LEFT_FRONT_IN2, HIGH);
+  }
+  ledcWrite(CHANNEL_LEFT_FRONT, abs(pwm) > VALUE_MAX ? VALUE_MAX : abs(pwm));
+}
+
+// 左後車輪の制御
+void backLeftMotor(int32_t pwm) {
+  if (pwm >= 0) {
+    digitalWrite(LEFT_BACK_IN1, HIGH);
+    digitalWrite(LEFT_BACK_IN2, LOW);
+  } else {
+    digitalWrite(LEFT_BACK_IN1, LOW);
+    digitalWrite(LEFT_BACK_IN2, HIGH);
+  }
+  ledcWrite(CHANNEL_LEFT_BACK, abs(pwm) > VALUE_MAX ? VALUE_MAX : abs(pwm));
+}
+
+// 右前車輪の制御
+void frontRightMotor(int32_t pwm) {
+  if (pwm >= 0) {
+    digitalWrite(RIGHT_FRONT_IN1, HIGH);
+    digitalWrite(RIGHT_FRONT_IN2, LOW);
+  } else {
+    digitalWrite(RIGHT_FRONT_IN1, LOW);
+    digitalWrite(RIGHT_FRONT_IN2, HIGH);
+  }
+  ledcWrite(CHANNEL_RIGHT_FRONT, abs(pwm) > VALUE_MAX ? VALUE_MAX : abs(pwm));
+}
+
+// 右後車輪の制御
+void backRightMotor(int32_t pwm) {
+  if (pwm >= 0) {
+    digitalWrite(RIGHT_BACK_IN1, HIGH);
+    digitalWrite(RIGHT_BACK_IN2, LOW);
+  } else {
+    digitalWrite(RIGHT_BACK_IN1, LOW);
+    digitalWrite(RIGHT_BACK_IN2, HIGH);
+  }
+  ledcWrite(CHANNEL_RIGHT_BACK, abs(pwm) > VALUE_MAX ? VALUE_MAX : abs(pwm));
+}
+
+/*
 #include <Arduino.h>
 #include <PS4Controller.h>
 
@@ -8,7 +163,7 @@
 //void        ledcWrite(uint8_t channel, uint32_t duty);
 
 
-/* 使うピンの定義 */
+使うピンの定義
 // Left
 const int LEFT_IN1 = 23;
 const int LEFT_IN2 = 22;
@@ -18,9 +173,9 @@ const int LEFT_IN4 = 3;
 const int RIGHT_IN1 = 4;
 const int RIGHT_IN2 = 16;
 const int RIGHT_IN3 = 15;
-const int RIGHT_IN4 = 2;
+const int RIGHT_IN4 = 2;*/
 
-/* チャンネルの定義 */
+/* チャンネルの定義 
 const int CHANNEL_0 = 0;
 const int CHANNEL_1 = 1;
 const int CHANNEL_2 = 2;
@@ -43,6 +198,7 @@ void frontLeftMotor(int32_t pwm);
 void backLeftMotor(int32_t pwm);
 void frontRightMotor(int32_t pwm);
 void backRightMotor(int32_t pwm);
+
 void forward(int32_t pwm);
 void backward(int32_t pwm);
 void right(int32_t pwm);
@@ -53,6 +209,7 @@ void rightBackward(int32_t pwm);
 void leftBackward(int32_t pwm);
 void clockWise(int32_t pwm);
 void counterClockWise(int32_t pwm);
+
 void brake();
 void coast();
 void LetsMoveMoter(int32_t pwm, double Cont_A, double Cont_B);
@@ -125,12 +282,12 @@ void init_pwm_setup() {
 }
 
 // 左前車輪
-void frontLeftMotor(int32_t pwm) {
+void frontLeftMotor(int32_t pwm) {//値が入ってくる
   if (pwm>=0){
-    positive = true; 
+    positive = true; //正回転
   }
   else{
-    positive = false;
+    positive = false;//負回転
   }
   if (pwm > VALUE_MAX || pwm < -VALUE_MAX) {
     pwm = VALUE_MAX;
@@ -329,6 +486,9 @@ void counterClockWise(int32_t pwm) {
 
 }*/
 
+
+//ブレーキ機能、どうする？
+/*
 // ブレーキ
 void brake() {
   ledcWrite(CHANNEL_0, VALUE_MAX);
@@ -351,4 +511,4 @@ void coast() {
   ledcWrite(CHANNEL_5, 0);
   ledcWrite(CHANNEL_6, 0);
   ledcWrite(CHANNEL_7, 0);
-}
+}*/
